f10c1666d31feb135478534b808991d094f965ea
Andrew Blanchette <blanc11@live.com>
2018-02-14 12:28:55 -0500

    Edit UserController to include root user view

--- lib/hello_web/controllers/user_controller.ex
+++ lib/hello_web/controllers/user_controller.ex
@@ -1,5 +1,9 @@
 defmodule HelloWeb.UserController do
 	use HelloWeb, :controller
+
+	def index(conn, _params) do
+		render conn, "index.html"
+	end	
 	
 	def new(conn, _params) do
 		render conn, "index.html"
